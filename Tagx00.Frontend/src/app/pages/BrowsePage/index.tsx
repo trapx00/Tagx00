e59d62d1e6bfb3734c6a5bf82dc339e283a1e455
surevil <445073309@qq.com>
2018-03-20 21:04:18 +0800

    some

--- Tagx00.Frontend/src/app/pages/BrowsePage/index.tsx
+++ Tagx00.Frontend/src/app/pages/BrowsePage/index.tsx
@@ -1,8 +1,6 @@
 import * as React from "react";
-import { Layout } from 'antd';
 import { SearchBar } from "../../components/SearchBar";
-
-const {Content} = Layout;
+import { BrowserMissionList } from "../../components/BrowserMissionList";
 
 const fillContent = {
   minHeight: '300px'
@@ -13,6 +11,7 @@ export class BrowsePage extends React.Component<any, any> {
     return (
       <div style={fillContent}>
         <SearchBar/>
+        <BrowserMissionList/>
       </div>
     )
   }
