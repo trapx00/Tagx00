79027bfa22a902bbb4551f54726cd1928adc4f75
surevil <445073309@qq.com>
2018-03-22 22:30:54 +0800

    edit some

--- Tagx00.Frontend/src/app/components/Browser/BrowserStore.tsx
+++ Tagx00.Frontend/src/app/components/Browser/BrowserStore.tsx
@@ -18,6 +18,7 @@ const smallerDiv = {
 };
 
 export class BrowserStore {
+  private static _maxLengthOfDescription = 100;
   @observable private _paused: boolean = true;
   @observable private _reverse: boolean = true;
   @observable private _listData: ListDataProps[] = [];
@@ -61,7 +62,7 @@ export class BrowserStore {
           </div>
         );
         listProp.startDate = new Date(missions[i].start).toDateString();
-        listProp.description = missions[i].description;
+        listProp.description = missions[i].description.length > BrowserStore._maxLengthOfDescription ? missions[i].description.substring(0, BrowserStore._maxLengthOfDescription) + "……" : missions[i].description;
         this._listData.push(listProp);
       }
     })
