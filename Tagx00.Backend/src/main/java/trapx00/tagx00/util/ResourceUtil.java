378906cff0c2dc6c1d928000a0abe54874d5f6d8
surevil <445073309@qq.com>
2018-03-26 10:46:14 +0800

    fileservice now can get all protected attributes

--- Tagx00.Backend/src/main/java/trapx00/tagx00/util/ResourceUtil.java
+++ Tagx00.Backend/src/main/java/trapx00/tagx00/util/ResourceUtil.java
@@ -2,11 +2,7 @@ package trapx00.tagx00.util;
 
 import trapx00.tagx00.MainApplication;
 
-import java.io.File;
-import java.net.URISyntaxException;
 import java.net.URL;
-import java.util.Arrays;
-import java.util.stream.Collectors;
 
 public class ResourceUtil {
     public static URL getResource(String path) {
@@ -15,17 +11,18 @@ public class ResourceUtil {
 
     public static String getFilePathUnderRootDirOfJarFileOrClassDir(String relativePath) {
         String path = MainApplication.class.getProtectionDomain().getCodeSource().getLocation().getPath();
-        String[] paths = path.split("!");
-        String filePath = paths[0];
+//        String path = MainApplication.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+//        String[] paths = path.split("!");
+//        String filePath = paths[0];
+//
+//        File jarFile = new File(paths[0]);
+//        String parentDir = Arrays.stream(jarFile.getParent().split(":"))
+//                .skip(1).collect(Collectors.joining(":"));
+//
+//
+//        System.out.println(parentDir);
+//        return parentDir + relativePath;
 
-        File jarFile = new File(paths[0]);
-        String parentDir = Arrays.stream(jarFile.getParent().split(":"))
-                .skip(1).collect(Collectors.joining(":"));
-
-
-        System.out.println(parentDir);
-        return parentDir + relativePath;
-
-
+        return path + ".." + relativePath;
     }
 }
