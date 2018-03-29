d0f827c90936d5ae3b0dc9b9d0d99d6c77947a8d
surevil <445073309@qq.com>
2018-03-29 19:13:46 +0800

    replace deperacted class

--- Tagx00.Backend/src/main/java/trapx00/tagx00/config/WebConfig.java
+++ Tagx00.Backend/src/main/java/trapx00/tagx00/config/WebConfig.java
@@ -5,13 +5,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.GsonHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
 @EnableWebMvc
-public class WebConfig extends WebMvcConfigurerAdapter {
+public class WebConfig implements WebMvcConfigurer {
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         GsonHttpMessageConverter msgConverter = new GsonHttpMessageConverter();
