9034e885a363fb748a4d8e6eb0bc0b48fb429b5f
米玲 <161250200@smail.nju.edu.cn>
2018-04-11 11:17:54 +0800

    try use Form to render Dashboard

--- Tagx00.Frontend/src/app/pages/SelfPage/DashboardPage.tsx
+++ Tagx00.Frontend/src/app/pages/SelfPage/DashboardPage.tsx
@@ -1,7 +1,37 @@
 import React from "react";
+import { Form } from 'antd';
 
 export class DashboardPage extends React.Component<any, any> {
+  //Q1：根据登录者身份显示不同的界面？路由
   render() {
-    return "dashboard";
+      const formItemLayout = {
+          labelCol: { span: 6 },
+          wrapperCol: { span: 14 },
+      };
+
+    return(
+
+        <Form>
+            <Form.Item
+                {...formItemLayout}
+                label="用户名:">
+                <p className="ant-form-text" id="userName">123</p>
+            </Form.Item>
+            <Form.Item
+                {...formItemLayout}
+                label="电子邮件:">
+                d
+            </Form.Item>
+            <Form.Item
+                {...formItemLayout}
+                label="经验:">
+                <p className="ant-form-text" id="userName">123</p>
+            </Form.Item>
+            <Form.Item
+                {...formItemLayout}
+                label="积分:">
+                <p className="ant-form-text" id="userName">123</p>
+            </Form.Item>
+        </Form>)
   }
 }
