39b18444fd3c6c2e347aa56bfc12d229dba4a9c8
米玲 <161250200@smail.nju.edu.cn>
2018-04-12 09:48:46 +0800

    continue dashboard

--- Tagx00.Frontend/src/app/pages/SelfPage/DashboardPage.tsx
+++ Tagx00.Frontend/src/app/pages/SelfPage/DashboardPage.tsx
@@ -1,37 +1,35 @@
 import React from "react";
 import { Form } from 'antd';
+import {Inject} from "react.di";
+import {observer} from "mobx-react";
+import {action} from "mobx";
+import {UserService} from "../../api/UserService";
+import {LocaleMessage} from "../../internationalization/components";
 
+@observer
 export class DashboardPage extends React.Component<any, any> {
-  //Q1：根据登录者身份显示不同的界面？路由
-  render() {
-      const formItemLayout = {
-          labelCol: { span: 6 },
-          wrapperCol: { span: 14 },
-      };
 
-    return(
+    @Inject userService: UserService;
 
-        <Form>
-            <Form.Item
-                {...formItemLayout}
-                label="用户名:">
-                <p className="ant-form-text" id="userName">123</p>
-            </Form.Item>
-            <Form.Item
-                {...formItemLayout}
-                label="电子邮件:">
-                d
-            </Form.Item>
-            <Form.Item
-                {...formItemLayout}
-                label="经验:">
-                <p className="ant-form-text" id="userName">123</p>
-            </Form.Item>
-            <Form.Item
-                {...formItemLayout}
-                label="积分:">
-                <p className="ant-form-text" id="userName">123</p>
-            </Form.Item>
-        </Form>)
+    mail : string;
+    currentEx : string;
+    nextEx : string ;
+
+    fetch = async () => {
+        const mail = await this.userService.login("","");
+    }
+
+    render() {
+
+      return[
+          <h1>
+              <LocaleMessage id={selfCenter.}/>
+          </h1>
+          <p>用户名:</p>,
+          <p>邮箱:</p>,
+          <p>经验:</p>,
+          <p>积分:</p>
+                  ]
+
   }
 }
