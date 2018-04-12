5517ef87d5416f28899fe5141195da2809b5bf5a
米玲 <161250200@smail.nju.edu.cn>
2018-04-12 10:53:52 +0800

    continue dashboard

--- Tagx00.Frontend/src/app/pages/SelfPage/DashboardPage.tsx
+++ Tagx00.Frontend/src/app/pages/SelfPage/DashboardPage.tsx
@@ -1,35 +1,44 @@
 import React from "react";
-import { Form } from 'antd';
 import {Inject} from "react.di";
 import {observer} from "mobx-react";
-import {action} from "mobx";
-import {UserService} from "../../api/UserService";
 import {LocaleMessage} from "../../internationalization/components";
+import {action, runInAction} from "mobx";
+import {UserStore} from "../../stores/UserStore";
+import {RequesterService} from "../../api/RequesterService";
+import {WorkerService} from "../../api/WorkerService";
 
 @observer
 export class DashboardPage extends React.Component<any, any> {
 
-    @Inject userService: UserService;
+    @Inject userStores: UserStore;
+    @Inject workerService: WorkerService;
+    @Inject requesterService: RequesterService;
 
-    mail : string;
-    currentEx : string;
-    nextEx : string ;
+    username = this.userStores.user.username;
+    email = this.userStores.user.email;
+    role = this.userStores.user.role;
+    currentEx = 0;
+    nextEx = 0 ;
 
-    fetch = async () => {
-        const mail = await this.userService.login("","");
+    async() {
+        const response = await this.workerService.
     }
 
+
     render() {
-
-      return[
-          <h1>
-              <LocaleMessage id={selfCenter.}/>
-          </h1>
-          <p>用户名:</p>,
-          <p>邮箱:</p>,
-          <p>经验:</p>,
-          <p>积分:</p>
-                  ]
-
+        switch(this.role){
+            case ""
+        }
+      return (
+         <div>
+             <h1>
+              <LocaleMessage id={"selfCenter.dashboard"}/>
+             </h1>
+             <p>用户名: {this.username} </p>
+             <p>邮箱: {this.email} </p>
+             <p>经验:</p>
+             <p>积分:</p>
+         </div>
+              )
   }
 }
