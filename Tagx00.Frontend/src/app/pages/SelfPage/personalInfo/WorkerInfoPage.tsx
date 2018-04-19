5608bb085b11d35b96b70d233040595af3c4d10f
米玲 <161250200@smail.nju.edu.cn>
2018-04-19 15:25:28 +0800

    continue

--- Tagx00.Frontend/src/app/pages/SelfPage/personalInfo/WorkerInfoPage.tsx
+++ Tagx00.Frontend/src/app/pages/SelfPage/personalInfo/WorkerInfoPage.tsx
@@ -5,8 +5,7 @@ import {Inject} from "react.di";
 import {LocaleMessage} from "../../../internationalization/components";
 import {AsyncComponent} from "../../../router/AsyncComponent";
 import {LevelStore} from "../../../stores/LevelStore";
-import Progress from "antd/es/progress/progress";
-import Row from "antd/es/grid/row";
+import { Progress } from 'antd';
 
 export class WorkerInfoPage extends React.Component<{},{}> {
     @Inject userStore: UserStore;
@@ -17,16 +16,15 @@ export class WorkerInfoPage extends React.Component<{},{}> {
         const info = await this.workerService.getWorkerInfo(this.userStore.user.username,this.userStore.token);
         const nextLevelExp = await this.levelStore.getNextLevelExp(info.exp); //下一等级总经验，不是还差
         const percent = (info.exp)*100/nextLevelExp;
-        return <div>
+        return (
+          <div>
             <p>用户名：{info.username}</p>
             <p>注册邮箱：{info.email}</p>
             <p>等级：{info.level} </p>
-            <div>
-              <Progress percent={percent} showInfo={false}/>
-            </div>
-          <p>{info.exp}/{nextLevelExp}</p>
+            <Progress percent={percent} showInfo={false}/> {info.exp}/{nextLevelExp}
             <p>积分：{info.credits }</p>
-        </div>
+
+        </div>);
     }
 
     render() {
@@ -36,6 +34,7 @@ export class WorkerInfoPage extends React.Component<{},{}> {
                     <LocaleMessage id={"selfCenter.personalInfo"}/>
                 </h1>
                 <AsyncComponent render={this.workerInfo}/>
+                <Progress percent={30} />
             </div>
         )
     }
