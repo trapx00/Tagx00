0acf907b6503e8e0d0a08ec6df4862830beb2668
米玲 <161250200@smail.nju.edu.cn>
2018-04-24 00:02:05 +0800

    Add prover

--- Tagx00.Frontend/src/app/pages/SelfPage/topic/TopicsManagementPage.tsx
+++ Tagx00.Frontend/src/app/pages/SelfPage/topic/TopicsManagementPage.tsx
@@ -6,12 +6,15 @@ import { LocaleMessage } from "../../../internationalization/components/index";
 import { TopicFetchResponse } from "../../../models/topic/response/TopicFetchResponse";
 import { TopicSave } from "../../../models/topic/TopicSave";
 import { TopicDelete } from "../../../models/topic/TopicDelete";
-import {Tag, Button} from "antd";
+import { Tag, Button, Popover, Input, Row, Col } from "antd";
 import { Topic } from "../../../models/topic/Topic";
 import { AsyncComponent } from "../../../router/AsyncComponent";
 import { DefinitionItem } from "../../../components/DefinitionItem/index";
+import { observable } from "mobx";
+import { TopicSaveVo } from "./TopicSaveVo";
 
 export class TopicsManagementPage extends React.Component<any,any> {
+  @observable topicSave: TopicSaveVo = new TopicSaveVo();
   @Inject userStore: UserStore;
   @Inject topicService: TopicService;
 
@@ -28,7 +31,7 @@ export class TopicsManagementPage extends React.Component<any,any> {
     this.setState({ tags });
   }
 
-  addTag() {
+  handleAdd() {
     const tag = this.state.tags;
     const topicId = tag.length + 1;
     tag.push({ topicId: topicId, value: `新标签${topicId}` });
@@ -38,14 +41,26 @@ export class TopicsManagementPage extends React.Component<any,any> {
   renderTopics = async() => {
     const topics = await this.topicService.getAllTopics();
     this.setState({tags: topics.topics});
+    const content = (
+      <div style={{width:105}}>
+        <Row>
+          <Col span={20}>
+            <Input size="small" placeholder="标签"/>
+          </Col>
+          <Col span={1} offset={2}>
+            <Button type="primary" size="small" onKeyPress={this.handleAdd}>确认</Button>
+          </Col>
+        </Row>
+      </div>
+    );
     return <div>{this.state.tags.map(tag =>
       <Tag key={tag.topicId} closable={tag.topicId !== 1} afterClose={() => this.handleClose(tag.topicId)}>
         {tag.value}
       </Tag>
     )}
-      <Button size="small" type="dashed" onClick={this.addTag}>
-        + 添加标签
-      </Button>
+      <Popover content={content} title={"添加"} trigger="click">
+        <Button size="small" type="dashed">+ 添加标签</Button>
+      </Popover>
     </div>
   }
 
