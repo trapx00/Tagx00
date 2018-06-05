8d9ba539d88ae5a78b625c82dff8fbbb0abdc65b
surevil <445073309@qq.com>
2018-06-05 13:59:53 +0800

    may complete regression version

--- Tagx00.MachineLearning/data/train/train.py
+++ Tagx00.MachineLearning/data/train/train.py
@@ -6,26 +6,68 @@ import tensorflow as tf
 total_train = 100
 learning_rate = 0.01
 training_epochs = 100
-batch_size = 10
+batch_size = 3
 n_size = 6
 
-weight = tf.Variable(tf.random_normal([n_size, n_size]))
-biases = tf.Variable(tf.random_normal([n_size]))
+train_data = []
+with open("../proval/train.txt", "r") as file:
+    all_data = file.readlines()
+    for j in range(all_data.__len__()):
+        data = json.loads(all_data[j].replace('\n', ""))
+        tags = data["response"]
+        targets = data["tags"]
+        tag = []
+        for i in range(n_size):
+            if i < tags.__len__():
+                if tags[i]["value"] in targets:
+                    tag.append([tags[i]["confidence"], 1])
+                else:
+                    tag.append([tags[i]["confidence"], 0])
+            else:
+                tag.append([0, 0])
+        sorted(tag, reverse=True)
+        train_data.append(tag)
 
-X = tf.placeholder(tf.float32, [n_size, None])
-Y = tf.placeholder(tf.float32, [n_size, None])
+test_data = []
+with open("../proval/test.txt", "r") as file:
+    all_data = file.readlines()
+    for j in range(all_data.__len__()):
+        data = json.loads(all_data[j].replace('\n', ""))
+        tags = data["response"]
+        targets = data["tags"]
+        tag = []
+        for i in range(n_size):
+            if i < tags.__len__():
+                if tags[i]["value"] in targets:
+                    tag.append([tags[i]["confidence"], 1])
+                else:
+                    tag.append([tags[i]["confidence"], 0])
+            else:
+                tag.append([0, 0])
+        sorted(tag, reverse=True)
+        test_data.append(tag)
+
+last_index = 0
 
 
 def next_train_batch():
-    X = np.empty([n_size, batch_size])
-    with open("../proval/train.txt", "r") as file:
-        data = json.loads(file.readline().replace('\n', ""))
-        tags = data["response"]
-        print(data)
+    batch_x = np.empty([n_size, batch_size])
+    batch_y = np.empty([n_size, batch_size])
+    for i in range(n_size):
+        for j in range(batch_size):
+            batch_x[i][j] = train_data[last_index + j][i][0]
+            batch_y[i][j] = train_data[last_index + j][i][1]
+    return batch_x, batch_y
 
 
 def next_test_batch():
-    pass
+    batch_x = np.empty([n_size, batch_size])
+    batch_y = np.empty([n_size, batch_size])
+    for i in range(n_size):
+        for j in range(batch_size):
+            batch_x[i][j] = test_data[0][i][0]
+            batch_y[i][j] = test_data[0][i][1]
+    return batch_x, batch_y
 
 
 def save_models():
@@ -36,15 +78,17 @@ def save_models():
 def compute_accuracy():
     batch_xs, batch_ys = next_test_batch()
     pred = sess.run(y_pred, feed_dict={X: batch_xs, Y: batch_ys})
-    one_hole_pred = tf.argmax(pred, 0)
-    one_hole_real = tf.argmax(batch_ys, 0)
-    print(tf.reduce_mean(tf.square(one_hole_pred - one_hole_real)))
+    print(sess.run(tf.reduce_mean(tf.square(pred - batch_ys))))
 
 
-next_train_batch()
+weight = tf.Variable(tf.random_normal([n_size, n_size]))
+biases = tf.Variable(tf.random_normal([n_size, batch_size]))
 
-y_pred = tf.multiply(weight, X) + biases
-cost = tf.reduce_mean(tf.abs(Y - y_pred))
+X = tf.placeholder(tf.float32, [n_size, None])
+Y = tf.placeholder(tf.float32, [n_size, None])
+
+y_pred = tf.sigmoid(tf.matmul(weight, X) + biases)
+cost = tf.reduce_mean(tf.square(Y - y_pred))
 
 optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
 sess = tf.Session()
@@ -54,7 +98,8 @@ total_batch = int(total_train / batch_size)
 for epoch in range(training_epochs):
     for i in range(total_batch):
         batch_xs, batch_ys = next_train_batch()
-        _, c = sess.run([optimizer, cost], feed_dict={X: batch_xs, Y: batch_ys})
+        _, c, pred = sess.run([optimizer, cost, y_pred], feed_dict={X: batch_xs, Y: batch_ys})
         print(c)
         if i % 10 == 0:
+            print("accuracy")
             compute_accuracy()
