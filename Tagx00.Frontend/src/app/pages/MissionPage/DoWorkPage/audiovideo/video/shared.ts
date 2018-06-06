22622e67db747c3b969cb8e9dbbc12823cae0711
Chen Junda <smallda@outlook.com>
2018-06-06 21:27:03 +0800

    adding video

---
+++ Tagx00.Frontend/src/app/pages/MissionPage/DoWorkPage/audiovideo/video/shared.ts
@@ -0,0 +1,16 @@
+import { VideoMissionDetail } from "../../../../../models/mission/video/VideoMission";
+import { VideoJob } from "../../../../../models/instance/video/job/VideoJob";
+import { WorkPageProps, WorkPageState } from "../../WorkPage";
+
+
+export interface VideoNotation<T extends VideoJob= VideoJob> extends Notation<T> {
+  videoUrl: string;
+
+}
+export interface VideoWorkPageProps<T extends VideoJob> extends WorkPageProps<VideoMissionDetail, T, VideoNotation<T>> {
+
+}
+
+export interface VideoWorkPageState<T extends VideoJob> extends WorkPageState<T, VideoNotation<T>>{
+
+}
